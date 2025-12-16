110503bc88afba9d1b5d3cee6e29de61ec529a81
pork-arie <bangcalearielangel70@gmail.com>
2025-12-16 21:23:38 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+        function reveal() {
+            var reveals = document.querySelectorAll(".reveal");
+            for (var i = 0; i < reveals.length; i++) {
+                var windowHeight = window.innerHeight;
+                var elementTop = reveals[i].getBoundingClientRect().top;
+                if (elementTop < windowHeight - 100) {
+                    reveals[i].classList.add("active");
+                }
+            }
+        }
+        window.addEventListener("scroll", reveal);
+        reveal(); // Run on load
